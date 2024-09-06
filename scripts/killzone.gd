7c5370bf64f51ab02ef9ae0ca443a13787c27a90
TJ Klint <timothyjklint@gmail.com>
2024-09-06 03:02:19 -0400

    feat: killzone now slows down game time for death effect.

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -4,10 +4,13 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You died, lol.")
+	Engine.time_scale = 0.4
+	body.get_node("CollisionShape2D2").queue_free()
 	timer.start()
 	
 	
 
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
