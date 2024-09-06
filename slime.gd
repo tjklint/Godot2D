dcf9934e1c7a5d36b199e2f2f8396b4dde4d6ae9
TJ Klint <timothyjklint@gmail.com>
2024-09-06 02:51:18 -0400

    fix: slime sprite flips upon direction swap.

--- slime.gd
+++ slime.gd
@@ -7,12 +7,18 @@ var direction = 1
 # Directional Movement (left or right)
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _process(delta: float) -> void:
 	if ray_cast_right.is_colliding():
 		direction = -1
+		animated_sprite.flip_h = true
+	
 	if ray_cast_left.is_colliding():
 		direction = 1
+		animated_sprite.flip_h = false
+
 	position.x += direction * ENEMY_SPEED * delta
+	
 	
 	
